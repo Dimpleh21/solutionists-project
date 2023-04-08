2067bfb53432931adf05f50d8e558c71d88b4106
Dimpleh21 <129992793+Dimpleh21@users.noreply.github.com>
2023-04-08 04:00:52 -0700

    Add files via upload

--- project/script.js
+++ project/script.js
@@ -24,11 +24,7 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 try {
-  const docRef = await addDoc(collection(db, "users"), {
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815
-  });
+  const docRef = await addDoc(collection(db, "users"));
   console.log("Document written with ID: ", docRef.id);
 } catch (e) {
   console.error("Error adding document: ", e);
